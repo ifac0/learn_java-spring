54ea94a24e342c40942fce4c471b9e1e24db98c6
Ivan <ivanfelipealves@gmail.com>
2020-02-02 03:56:53 -0300

    :sparkles: Adiciona rota de criar Jedi;
    Signed-off-by: Ivan <ivanfelipealves@gmail.com>

--- spring-web-mvc/src/main/java/com/stdy/springwebmvc/rest/JediResource.java
+++ spring-web-mvc/src/main/java/com/stdy/springwebmvc/rest/JediResource.java
@@ -5,10 +5,9 @@ import com.stdy.springwebmvc.model.Jedi;
 import com.stdy.springwebmvc.repository.JediRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,5 +30,10 @@ public class JediResource {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping("/api/jedi")
+    public Jedi createJedi(@Valid @ModelAttribute Jedi jedi){
+        return  repository.save(jedi);
     }
 }

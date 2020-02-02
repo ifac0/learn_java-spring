266a49e1419eeb12c88c4006a9f6ece3930b8de4
Ivan <ivanfelipealves@gmail.com>
2020-02-02 01:44:48 -0300

    :art: Injeta a variável com @Value em AppController;
    Signed-off-by: Ivan <ivanfelipealves@gmail.com>

--- src/main/java/com/stdy/springboot/AppController.java
+++ src/main/java/com/stdy/springboot/AppController.java
@@ -10,8 +10,16 @@ public class AppController {
     @Value("${app.message}")
     private String appMessage;
 
+    @Value("${ENV_DB_URL:NULL}")
+    private String dbEnvironmentVariable;
+
     @GetMapping("/")
     public String getAppMessage(){
         return appMessage;
+    }
+
+    @GetMapping("/envVariable")
+    public String getEnvironmentVariable(){
+        return "A seguinte variável do ambiente foi passada: " + dbEnvironmentVariable;
     }
 }
